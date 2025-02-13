eb8834bf1c3d00ee4347f4a08b9511bf5b21a016
Mau <maufo1806@gmail.com>
2025-02-12 18:15:12 -0600

    Añadido de verificador del texto

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 function agregarAmigo() {
-    //Se llama a la función y se guarda por ID
-    let nombreDeAmigos = document.getElementById('amigo').value.trim();
-    console.log(nombreDeAmigos); 
-    if (nombreDeAmigos === ''){
-        alert("Por favor indique un nombre");
-        return;
-    } else{
-     amigos.push(nombreDeAmigos);
-     document.getElementById("amigo").value ="";
-        console.log(amigos);
+    function verificadorDeTexto(){
+        //Se llama a la función y se guarda por ID, se añade otra una función adicional para verificar que se ingrese un nombre válido
+        let nombreDeAmigos = document.getElementById('amigo').value.trim();
+        if (isNaN(nombreDeAmigos)) {
+            console.log ("Texto = letras");
+        }else {
+            console.log ("Texto = num");
+        }
+        if (nombreDeAmigos === ""){
+            alert("Por favor indicar un nombre válido");
+            return;
+        }else {
+         amigos.push(nombreDeAmigos);
+         document.getElementById("amigo").value = "";
+         console.log(amigos);
+        }
     }
-} 
+     verificadorDeTexto();    
+    }
+        
+    
