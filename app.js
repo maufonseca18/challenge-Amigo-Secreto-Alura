ebf1fedf38170224cf797328781728771b4b289a
Mau <maufo1806@gmail.com>
2025-02-13 12:17:39 -0600

    Se crea el array para que muestre la lista de amigos

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let amigos = [];
 function agregarAmigo() {
     function verificadorDeTexto(){
         //Se llama a la función y se guarda por ID, se añade otra una función adicional para verificar que se ingrese un nombre válido
-        let nombreDeAmigos = document.getElementById("amigo").value.trim(); 
+        let nombreDeAmigos = document.getElementById("amigo").value.trim(); //selecciona lista donde se mostrarán los amigos
         const soloLetras  = /^[a-zA-Z\s]+$/; // Expresión regular que verifica carácteres especiales
         if (nombreDeAmigos === ""){
             alert("Por favor ingrese un nombre válido"); //si el valor ingresado es nulo se genera una alerta al usuario 
@@ -13,6 +13,7 @@ function agregarAmigo() {
             amigos.push(nombreDeAmigos);
             document.getElementById("amigo").value= "";
             console.log(amigos);
+            mostrarAmigos(); //LLamar a la función para mostrar la lista. 
         }else {
             alert("Por favor indique un nombre válido");//en caso de ingresar un número se generara un alerta al usuario 
             document.getElementById("amigo").value= ""; //Limpiar la caja de texto después de un error o valor inválido.
@@ -20,4 +21,15 @@ function agregarAmigo() {
         } 
     }
     verificadorDeTexto();
+}
+function mostrarAmigos(){
+    //se obtiene el elemento de la lista de amigos
+    const listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";//limpiar la lista existente 
+    //Itera sobre el arreglo y agrega os alementos a la lista
+    for (let i = 0; i < amigos.length; i++){
+        const nuevoAmigo = document.createElement("li");
+        nuevoAmigo.textContent = amigos[i];
+        listaAmigos.appendChild(nuevoAmigo);
+    }
 }
