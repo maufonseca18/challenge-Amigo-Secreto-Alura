a096b2327bb8a8d5f9f50c82571dd213c2f62976
Mau <maufo1806@gmail.com>
2025-02-12 19:36:08 -0600

    Se añade al código una variable para detectar si el texto es válido

--- app.js
+++ app.js
@@ -3,22 +3,19 @@ let amigos = [];
 function agregarAmigo() {
     function verificadorDeTexto(){
         //Se llama a la función y se guarda por ID, se añade otra una función adicional para verificar que se ingrese un nombre válido
-        let nombreDeAmigos = document.getElementById('amigo').value.trim();
-        if (isNaN(nombreDeAmigos)) {
-            console.log ("Texto = letras");
-        }else {
-            console.log ("Texto = num");
-        }
+        let nombreDeAmigos = document.getElementById("amigo").value.trim();
+
         if (nombreDeAmigos === ""){
-            alert("Por favor indicar un nombre válido");
+            alert("Por favor ingrese un nombre válido"); //si el valor ingresado es nulo se genera una alerta al usuario 
             return;
-        }else {
-         amigos.push(nombreDeAmigos);
-         document.getElementById("amigo").value = "";
-         console.log(amigos);
+        }else if (isNaN(nombreDeAmigos)){ //si el valor ingresado es válido entonces es alamcenado
+            amigos.push(nombreDeAmigos);
+            document.getElementById("amigo").value="";
+            console.log(amigos);
+        }else{
+            alert("Por favor indique un nombre válido");//en caso de ingresar un número se generara un alerta al usuario 
+            return;
         }
     }
-     verificadorDeTexto();    
-    }
-        
-    
+    verificadorDeTexto();
+}
