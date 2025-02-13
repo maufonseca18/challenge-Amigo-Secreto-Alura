96db269ca308a609b504635b86d86a0cb556740e
Mau <maufo1806@gmail.com>
2025-02-13 11:14:35 -0600

    Se añade verificador de carácteres válidos y solo letras

--- app.js
+++ app.js
@@ -3,19 +3,21 @@ let amigos = [];
 function agregarAmigo() {
     function verificadorDeTexto(){
         //Se llama a la función y se guarda por ID, se añade otra una función adicional para verificar que se ingrese un nombre válido
-        let nombreDeAmigos = document.getElementById("amigo").value.trim();
-
+        let nombreDeAmigos = document.getElementById("amigo").value.trim(); 
+        const soloLetras  = /^[a-zA-Z\s]+$/; // Expresión regular que verifica carácteres especiales
         if (nombreDeAmigos === ""){
             alert("Por favor ingrese un nombre válido"); //si el valor ingresado es nulo se genera una alerta al usuario 
+            document.getElementById("amigo").value= "";
             return;
-        }else if (isNaN(nombreDeAmigos)){ //si el valor ingresado es válido entonces es alamcenado
+        }else if (isNaN(nombreDeAmigos) && soloLetras.test(nombreDeAmigos)) { //si el valor ingresado es válido entonces es alamcenado
             amigos.push(nombreDeAmigos);
-            document.getElementById("amigo").value="";
+            document.getElementById("amigo").value= "";
             console.log(amigos);
-        }else{
+        }else {
             alert("Por favor indique un nombre válido");//en caso de ingresar un número se generara un alerta al usuario 
+            document.getElementById("amigo").value= ""; //Limpiar la caja de texto después de un error o valor inválido.
             return;
-        }
+        } 
     }
     verificadorDeTexto();
 }
